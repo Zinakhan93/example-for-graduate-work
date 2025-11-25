1bbc9bd096c1970378a395d829b57586969c0f3b
Zinakhan93 <Zinakhan93@mail.ru>
2025-11-25 23:31:13 +0300

    Добавлен interface AdService интерфейс объявлений

--- src/main/java/ru/skypro/homework/entity/AdEntity.java
+++ src/main/java/ru/skypro/homework/entity/AdEntity.java
@@ -8,7 +8,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
-@Data
 @Getter
 @Setter
 @AllArgsConstructor // создает конструктор
---
+++ src/main/java/ru/skypro/homework/service/AdService.java
@@ -0,0 +1,20 @@
+package ru.skypro.homework.service;
+
+import org.springframework.web.multipart.MultipartFile;
+import ru.skypro.homework.dto.Ad;
+import ru.skypro.homework.dto.Ads;
+import ru.skypro.homework.dto.CreateOrUpdateAd;
+import ru.skypro.homework.dto.ExtendedAd;
+
+import java.io.IOException;
+
+public interface AdService {
+    // Объявление методов для работы с объявлениями
+    Ads getAllAds(); // Получить все объявления
+    Ad addAd(CreateOrUpdateAd properties, MultipartFile image, String username) throws IOException; // Добавить объявление
+    ExtendedAd getAd(Integer id); // Получить объявление по ID
+    void removeAd(Integer id); // Удалить объявление
+    Ad updateAd(Integer id, CreateOrUpdateAd updateAd); // Обновить объявление
+    Ads getAdsMe(String username); // Получить объявления текущего пользователя
+    byte[] updateAdImage(Integer id, MultipartFile image) throws IOException; // Обновить изображение объявления
+}
